ca6915ee149247f2d8cf209567ffa251e10cdb83
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-09 02:53:50 +0300

    Add joi validations in every func

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,84 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-const contacts = require("../../models/contacts");
+const contacts = require("../../controllers/contacts");
 
 require("colors");
 
-const handleValidations = require("../../controllers/handleValidations");
-router.get("/", async (req, res, next) => {
-  // the route will be '3000:/api/contacts[get-path]
-  try {
-    const data = await contacts.listContacts();
-    res.send(data);
-  } catch (error) {
-    res.status(500).send(error);
-    throw new Error(`${error.message}`.red);
-  }
-});
+router.get("/", contacts.getAll);
 
-router.get("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
+router.get("/:contactId", contacts.getById);
 
-  try {
-    const data = await handleValidations.isContactExist(res, contactId);
+router.post("/", contacts.addContact);
 
-    res.send(data);
-  } catch (error) {
-    res.status(500).send(error);
-    throw new Error(`${error.message}`.red);
-  }
-});
+router.delete("/:contactId", contacts.deleteContact);
 
-router.post("/", async (req, res, next) => {
-  const { name, email, phone } = req.body;
-
-  try {
-    handleValidations.checkRequiredFields(res, name, email, phone);
-    await handleValidations.isContactWithSameFields(res, email, phone);
-
-    const contact = await contacts.addContact(req.body);
-    res.status(201).json(contact);
-
-    res.end("Contact added successfully!");
-  } catch (error) {
-    res.status(500).end("Error adding contact");
-    throw new Error(`${error.message}`.red);
-  }
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const id = req.params.contactId;
-
-    await handleValidations.isContactExist(res, id);
-    await contacts.removeContact(id);
-
-    res.send("Contact is successfully removed!");
-    next();
-  } catch (error) {
-    res.status(500).send(error);
-    throw new Error(`${error.message}`.red);
-  }
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  const id = req.params.contactId;
-  const { name, email, phone } = req.body;
-  if (!name && !email && !phone) {
-    res.status(400).json({ message: "Missing fields" }).end();
-    return;
-  }
-  try {
-    await handleValidations.isContactExist(res, id);
-
-    const updatedContact = await contacts.updateContact(id, req.body);
-
-    res.status(200).json({ updatedContact });
-  } catch (error) {
-    res.status(500).send(error.message);
-    throw new Error(`${error.message}`.red);
-  }
-});
+router.put("/:contactId", contacts.updateContact);
 
 module.exports = router;
