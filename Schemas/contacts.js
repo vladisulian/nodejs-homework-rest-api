ab70501238ab7094ef0f6256a70a6017e7927f09
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-10 16:50:09 +0300

    Edit contactsScheme(validation)

--- Schemas/contacts.js
+++ Schemas/contacts.js
@@ -1,11 +1,9 @@
 const Joi = require("joi");
 
 const contactsSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string()
-    .pattern(/^\+?[0-9]{7,14}$/)
-    .required(),
+  name: Joi.string().min(3).max(30),
+  email: Joi.string().email(),
+  phone: Joi.string().pattern(/^\+?[0-9]{7,14}$/),
 });
 
 module.exports = {
