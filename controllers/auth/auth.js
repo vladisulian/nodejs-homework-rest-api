9f38e0447fa815c5be6ecc7a095e25be64b4ca15
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-25 12:29:20 +0300

    Add try/catch with creating logic (auth-controller)

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -1,6 +1,22 @@
+const User = require("../../Schemas/user");
+require("colors");
 
-const register = (req, res, next) => {
-  res.json({ message: "Register" });
+const register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = { name, email, password };
+
+    console.log(req.body);
+
+    await User.create(user);
+
+    res.status(201).end(user);
+  } catch (error) {
+    console.error(`${error}`.red);
+
+    return res.status(409).json({ message: error.message });
+  }
 };
 
 const login = (req, res, next) => {
