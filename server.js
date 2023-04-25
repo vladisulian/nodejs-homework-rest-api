4361abb38a27ad589c3450b28023403a35665b41
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-25 12:20:44 +0300

    Add PORT from .env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { DB } = require("./mongoDB");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running   on ${PORT}`);
+  console.log(`Server is running on port ${PORT}!`);
 
   DB(); // ? database connection
 });
