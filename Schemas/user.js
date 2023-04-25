14b1cd6074c4d14a52c9f1ed5df75d06637228f8
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-25 12:20:03 +0300

    Renamed users to user

---
+++ Schemas/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const user = new mongoose.Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+
+    //   owner: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'user',
+    //   }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const User = mongoose.model("User", user);
+
+module.exports = User;
