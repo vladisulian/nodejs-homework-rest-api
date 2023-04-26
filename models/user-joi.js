ad3af5ef62a020bd9141226636f99b18db97c5fd
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-27 00:59:09 +0300

    Update user-joi.js

--- models/user-joi.js
+++ models/user-joi.js
@@ -5,10 +5,12 @@ function joiUser(req, res, next) {
   const userSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(3),
+    token: Joi.string(),
+    subscription: Joi.string(),
   });
 
   const { error } = userSchema.validate(req.body);
-  
+
   if (error) {
     console.error(`Joi Error: ${error.message}`.red);
     res.status(400).json({ message: error }).end();
