6ce18951f177495ea5a6dab8776e9a197616048b
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-28 01:12:25 +0300

    Add a valid value for subscription

--- models/user-joi.js
+++ models/user-joi.js
@@ -20,4 +20,20 @@ function joiUser(req, res, next) {
   next();
 }
 
-module.exports = joiUser;
+function joiSubscription(req, res, next) {
+  const userSchema = Joi.object({
+    subscription: Joi.string().valid("starter", "pro", "business").required(),
+  });
+
+  const { error } = userSchema.validate(req.body);
+
+  if (error) {
+    console.error(`Joi Error: ${error}`.red);
+    res.status(400).json({ message: error.message }).end();
+    return;
+  }
+
+  next();
+}
+
+module.exports = { joiUser, joiSubscription };
