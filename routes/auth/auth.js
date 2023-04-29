97738c9aaa10ceaddcf503e1dc016f901e0ba8fa
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-04-29 19:44:57 +0300

    Add a route for updating an avatar

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -13,8 +13,11 @@ router.post("/register", joiUser, alreadyRegistered, AuthControllers.register);
 router.post("/login", joiUser, isUserExist, AuthControllers.login);
 
 router.post("/logout", joiUser, auth, AuthControllers.logout);
+
 router.get("/current", joiUser, auth, AuthControllers.getCurrentUser);
 
 router.patch("/", auth, joiSubscription, AuthControllers.updateSubscription);
+
+router.patch("/avatars", auth, AuthControllers.updateAvatar);
 
 module.exports = router;
