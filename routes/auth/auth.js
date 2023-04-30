0c4ad88ddc07bcb7439b1babd263e6fb94d27474
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-05-01 00:31:28 +0300

    Add tmpDir function. Made it dynamically. He's create tmp dir, if it doesn't exist.

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,11 +1,32 @@
 const router = require("express").Router();
 
 const path = require("path");
+const { existsSync } = require("fs");
+const { mkdir } = require("fs/promises");
 const crypto = require("crypto");
+
+const AuthControllers = require("../../controllers/auth/auth");
+const {
+  alreadyRegistered,
+  isUserExist,
+  auth,
+} = require("../../middleware/auth");
+const { joiUser, joiSubscription } = require("../../models/user-joi");
 
 const multer = require("multer");
 
-const tmpDir = path.join(__dirname, "..", "..", "tmp");
+const tmpDir = async (req, file, cb) => {
+  const directory = path.join(__dirname, "..", "..", "tmp");
+  const directoryExist = existsSync(directory);
+
+  //* cb - callback. The second argument - returning value
+  if (!directoryExist) {
+    await mkdir(directory);
+    cb(null, directory);
+  }
+
+  cb(null, directory);
+};
 const storage = multer.diskStorage({
   destination: tmpDir, // ? avatars saving directory
   filename: (req, file, cb) => {
@@ -20,14 +41,6 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
-
-const AuthControllers = require("../../controllers/auth/auth");
-const {
-  alreadyRegistered,
-  isUserExist,
-  auth,
-} = require("../../middleware/auth");
-const { joiUser, joiSubscription } = require("../../models/user-joi");
 
 router.post("/register", joiUser, alreadyRegistered, AuthControllers.register);
 
