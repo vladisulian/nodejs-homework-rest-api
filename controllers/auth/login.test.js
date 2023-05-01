42b9d4dd28635a59d43077009c6bd60ee1adde06
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-05-02 01:35:48 +0300

    Add unit test for login function

--- controllers/auth/login.test.js
+++ controllers/auth/login.test.js
@@ -8,28 +8,48 @@ describe("login", () => {
     body: { password: "password" },
     user: { password: "password", _id: "123" },
   };
-  const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+    token: jest.fn().mockReturnThis(),
+    user: {
+      email: "example@mail.com",
+      subscription: "pro",
+    },
+  };
   const next = jest.fn();
 
-  test("res.status === 200", async () => {
-    jest.spyOn(User, "findByIdAndUpdate").mockResolvedValueOnce();
+  jest.spyOn(User, "findByIdAndUpdate").mockResolvedValueOnce();
 
-    const jwtSpy = jest.spyOn(jwt, "sign").mockReturnValueOnce("token");
+  const jwtSpy = jest.spyOn(jwt, "sign").mockReturnValueOnce("token");
 
-    const bcryptCompareSpy = jest
-      .spyOn(bcrypt, "compare")
-      .mockImplementationOnce((password, userPassword, callback) => {
-        callback(null, true);
-      });
+  const bcryptCompareSpy = jest
+    .spyOn(bcrypt, "compare")
+    .mockImplementationOnce((password, userPassword, callback) => {
+      callback(null, true);
+    });
 
-    await login(req, res, next);
+  login(req, res, next);
 
+  test("res.status === 200", () => {
     expect(res.status).toHaveBeenCalledWith(200);
 
     jwtSpy.mockRestore();
     bcryptCompareSpy.mockRestore();
     User.findByIdAndUpdate.mockRestore();
   });
-  //   test("In response should be token and object(user) with 2 fields: email, subscription with STRING data-type.", () => {});
-  //   test("", () => {});
+
+  test("In response should be token ", async () => {
+    expect(res).toHaveProperty("token");
+  });
+  test(`In response should be user(object) with 2 fields: email, subscription with STRING data-type.`, async () => {
+    expect(res).toHaveProperty("user");
+    expect(typeof res.user).toBe("object");
+
+    expect(res.user).toHaveProperty("email");
+    expect(typeof res.user.email).toBe("string");
+
+    expect(res.user).toHaveProperty("subscription");
+    expect(typeof res.user.subscription).toBe("string");
+  });
 });
