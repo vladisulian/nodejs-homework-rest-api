b0fe521604438c8334b289b48a6948e8acf5a3a8
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-05-01 03:25:41 +0300

    Add login.test.js

--- controllers/auth/login.test.js
+++ controllers/auth/login.test.js
@@ -1,19 +1,35 @@
 const { login } = require("./auth");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/user");
+const bcrypt = require("bcrypt");
 
 describe("login", () => {
-  test("Status-code should be 200. ", async () => {
-    const req = {
-      body: { password: "password" },
-      user: { password: "password" },
-    };
-    const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-    const next = jest.fn(); // ? заглушка
+  const req = {
+    body: { password: "password" },
+    user: { password: "password", _id: "123" },
+  };
+  const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+  const next = jest.fn();
+
+  test("res.status === 200", async () => {
+    jest.spyOn(User, "findByIdAndUpdate").mockResolvedValueOnce();
+
+    const jwtSpy = jest.spyOn(jwt, "sign").mockReturnValueOnce("token");
+
+    const bcryptCompareSpy = jest
+      .spyOn(bcrypt, "compare")
+      .mockImplementationOnce((password, userPassword, callback) => {
+        callback(null, true);
+      });
 
     await login(req, res, next);
 
-    expect(res.status).toBe(200);
-  });
+    expect(res.status).toHaveBeenCalledWith(200);
 
-  test("In response should be token and object(user) with 2 fields: email, subscription with STRING data-type.", () => {});
-  test("", () => {});
+    jwtSpy.mockRestore();
+    bcryptCompareSpy.mockRestore();
+    User.findByIdAndUpdate.mockRestore();
+  });
+  //   test("In response should be token and object(user) with 2 fields: email, subscription with STRING data-type.", () => {});
+  //   test("", () => {});
 });
